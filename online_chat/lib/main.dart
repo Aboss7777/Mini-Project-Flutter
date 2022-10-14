844db1f3f22579f0ba9c74504bbbe4746dd79f0d
Aboss7777 <96716605+Aboss7777@users.noreply.github.com>
2022-10-14 23:59:57 +0530

    Add files via upload

--- online_chat/lib/main.dart
+++ online_chat/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
+import 'package:online_chat/helper/helper_function.dart';
 import 'package:online_chat/pages/home_page.dart';
 import 'package:online_chat/shared/constants.dart';
+import 'package:online_chat/pages/auth/login_page.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -30,6 +32,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+   bool _isSignedIn = false;
 
   @override
   void initState() {
@@ -37,11 +40,23 @@ class _MyAppState extends State<MyApp> {
     getUserLoggedInStatus();
   }
 
+  getUserLoggedInStatus() async {
+    await HelperFunctions.getUserLoggedInStatus().then((value) {
+      if (value != null){
+          _isSignedIn = value;
+      }
+    });
+  }
+      
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+          primaryColor: Constants().primaryColor,
+          scaffoldBackgroundColor: Color.fromARGB(195, 168, 164, 186)),
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: _isSignedIn ? const HomePage() : const LoginPage(),
     );
 
   }
---
+++ online_chat/lib/widgets/widgets.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+const textInputDecoration = InputDecoration(
+  labelStyle: TextStyle(color: Color.fromARGB(255, 250, 245, 245), fontWeight: FontWeight.w300),
+  focusedBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Color.fromARGB(191, 132, 57, 232), width: 2),
+  ),
+  enabledBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Color.fromARGB(191, 132, 57, 232), width: 2),
+  ),
+  errorBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Color.fromARGB(191, 132, 57, 232), width: 2),
+  ),
+);
